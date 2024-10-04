a98e1ae389a70a0a6765b20f73d23b6804e9cffe
bbmattieu9 <bbmattieu9@gmail.com>
2024-10-03 18:53:28 -0700

    [ADD] aDD to AuthStore, types and shared folder

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/action';
+import { RegisterRequestInteface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  constructor(private fb: FormBuilder, private store: Store) {}
 
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log('[ __Form Value__]:', this.form.getRawValue());
+    const request: RegisterRequestInteface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(register({ request }));
+  }
 }
