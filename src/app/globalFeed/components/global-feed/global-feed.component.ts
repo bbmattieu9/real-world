8ee69109a7ac8fe26cb183086937cb2d0038ecfc
bbmattieu9 <bbmattieu9@gmail.com>
2024-10-16 22:32:07 -0700

    [Add] Banner component to globalFeed component to display app header text

--- src/app/globalFeed/components/global-feed/global-feed.component.ts
+++ src/app/globalFeed/components/global-feed/global-feed.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedComponent } from '../../../shared/components/feed/feed.component';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from '../../../shared/components/banner/banner.component';
 
 @Component({
   selector: 'app-global-feed',
   standalone: true,
-  imports: [FeedComponent, CommonModule],
+  imports: [FeedComponent,BannerComponent, CommonModule],
   templateUrl: './global-feed.component.html',
   styleUrl: './global-feed.component.scss'
 })
