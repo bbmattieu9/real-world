d37e5d642bbfb6259da91192bdf6ef05bb0b6e67
bbmattieu9 <bbmattieu9@gmail.com>
2024-11-04 14:53:05 -0800

    [Add] article logic with Store

--- src/app/article/article.routes.ts
+++ src/app/article/article.routes.ts
@@ -1,9 +1,17 @@
 import { Route } from "@angular/router";
 import { ArticleComponent } from "./components/article/article.component";
+import { provideEffects } from "@ngrx/effects";
+import * as articleEffects from './store/effects';
+import { provideState } from "@ngrx/store";
+import { articleFeatureKey, articleReducer } from "./store/reducer";
 
 export const ARTICLE_ROUTES: Route[] = [
     {
         path: '',
-        component: ArticleComponent
+        component: ArticleComponent,
+        providers: [
+            provideEffects(articleEffects),
+            provideState(articleFeatureKey, articleReducer)
+        ]
     }
 ]
--- src/app/shared/data-access/article.service.ts
+++ src/app/shared/data-access/article.service.ts
@@ -1,24 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, delay, tap, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { GetFeedResponseInterface } from '../components/feed/types/getFeedResponse.interface';
 import { ArticleInterface } from '../types/article.interface';
 import { ArticleResponseInterface } from '../types/articleResponse.interface';
 
-
-
-
-// body: string;
-//   createdAt: string;
-//   description: string;
-//   favorited: boolean;
-//   favoritesCount: number;
-//   slug: string;
-//   tagList: PopularTagType[];
-//   title: string;
-//   updatedAt: string;
-//   author: ProfileInterface;
 
 export const DUMMY_ARTICLES: ArticleInterface =
     {
@@ -40,61 +26,6 @@ export const DUMMY_ARTICLES: ArticleInterface =
         following: false,
       },
     }
-    
-    // {
-    //   body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    //   createdAt: 'Feb 15 1985',
-    //   description:
-    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    //   favorited: true,
-    //   favoritesCount: 5,
-    //   slug: 'sample-body-body-2',
-    //   tagList: ['technology', 'science'],
-    //   title: 'What is the whole essence of man?',
-    //   updatedAt: 'Feb 15 2021',
-    //   author: {
-    //     username: 'Alice Johnson',
-    //     bio: 'Tech enthusiast',
-    //     image: 'alice.jpg',
-    //     following: true,
-    //   },
-    // },
-    // {
-    //   body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    //   createdAt: 'Mar 10 1990',
-    //   description:
-    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    //   favorited: false,
-    //   favoritesCount: 0,
-    //   slug: 'sample-body-body-3',
-    //   tagList: ['lifestyle'],
-    //   title: 'The cost of inner bliss, an anatomy of core existence',
-    //   updatedAt: 'Mar 10 2021',
-    //   author: {
-    //     username: 'John Doe',
-    //     bio: 'Travel blogger',
-    //     image: 'john.jpg',
-    //     following: false,
-    //   },
-    // },
-    // {
-    //   body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    //   createdAt: 'Apr 25 1995',
-    //   description:
-    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    //   favorited: true,
-    //   favoritesCount: 10,
-    //   slug: 'sample-body-body-4',
-    //   tagList: ['food', 'health'],
-    //   title: 'To what extent can man thrive?',
-    //   updatedAt: 'Apr 25 2021',
-    //   author: {
-    //     username: 'Emma Brown',
-    //     bio: 'Food lover',
-    //     image: 'emma.jpg',
-    //     following: true,
-    //   },
-    // },
   
 
 @Injectable({
